be0a6914276680885c94f3a2d7edffc68a17dcea
GG <graczu7@wp.pl>
2018-10-10 19:06:06 +0200

    first web page

--- src/main/java/com/example/currencyexchange/controller/ExchangeViewController.java
+++ src/main/java/com/example/currencyexchange/controller/ExchangeViewController.java
@@ -1,4 +1,12 @@
 package com.example.currencyexchange.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ExchangeViewController {
+    @GetMapping("/")
+    public String getReview(){
+        return "index";
+    }
 }
