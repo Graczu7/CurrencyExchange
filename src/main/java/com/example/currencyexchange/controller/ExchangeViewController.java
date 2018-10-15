33e579a2a835b5b536ea9fff143769086680cb4b
GG <graczu7@wp.pl>
2018-10-15 20:55:45 +0200

    currency

--- src/main/java/com/example/currencyexchange/controller/ExchangeViewController.java
+++ src/main/java/com/example/currencyexchange/controller/ExchangeViewController.java
@@ -1,12 +1,15 @@
 package com.example.currencyexchange.controller;
 
+import com.example.currencyexchange.model.Currency;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExchangeViewController {
     @GetMapping("/")
-    public String getReview(){
+    public String getReview(Model model){
+        model.addAttribute("currencies", Currency.values());
         return "/index.jsp";
     }
 }
--- src/main/java/com/example/currencyexchange/model/Currency.java
+++ src/main/java/com/example/currencyexchange/model/Currency.java
@@ -1,15 +1,6 @@
 package com.example.currencyexchange.model;
 
 public enum Currency {
-    ;
-
-    private String code;
-
-    Currency(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
+    THB, USD, AUD, HKD, CAD, NZD, SGD, EUR, HUF, CHF, GBP, UAH, JPY, CZK, DKK, ISK, NOK,
+    SEK, HRK, RON, BGN, TRY, ILS, CLP, MXN, PHP, ZAR, BRL, MYR, RUB, IDR, INR, KRW, CNY, XDR
 }
